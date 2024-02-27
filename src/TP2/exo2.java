5888c4696319d28570134dddc67a672a28ac7b6b
Lina <laaraichlina@gmail.com>
2024-02-27 21:52:06 +0100

    added TP2 exercice 2

---
+++ src/TP2/exo2.java
@@ -0,0 +1,18 @@
+package TP2;
+
+import java.util.Scanner;
+
+public class exo2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("X = ");
+        int x = s.nextInt();
+        System.out.print("N = ");
+        int n = s.nextInt();
+        s.close();
+        double power = 1;
+        for(int i = 1;i <= n; i++) power *= x;
+        System.out.println(power);
+
+    }
+}
