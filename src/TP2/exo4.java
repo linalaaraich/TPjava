eaf5530effbb80125cc791f0391e9e2d12c463cd
Lina <laaraichlina@gmail.com>
2024-02-28 15:10:57 +0100

    exo4

---
+++ src/TP2/exo4.java
@@ -0,0 +1,17 @@
+package TP2;
+
+import java.util.Scanner;
+
+public class exo4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = s.nextInt();
+        s.close();
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++) System.out.print("*");
+        System.out.println();
+        }
+
+    }
+}
