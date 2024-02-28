f3753bb72260c1fc09d3d1488667585ab72a47cb
Lina <laaraichlina@gmail.com>
2024-02-28 14:54:20 +0100

    exo3 serie 2

---
+++ src/TP2/exo3.java
@@ -0,0 +1,18 @@
+package TP2;
+
+import java.util.Scanner;
+public class exo3 {
+    public static void main(String[] args){
+        //some des N premiers termes de la serie harmonique ??? 1 + 1/2 + 1/3 + 1/4 + 1/5 +...+1/n
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        s.close();
+
+        int h = 0;
+        while(n <=1){
+            h += 1/n;
+            n--;
+        }
+
+    }
+}
