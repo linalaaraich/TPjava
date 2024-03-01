12674caff6fe28588a22c7d4c7e08fbfafa5986a
Lina <laaraichlina@gmail.com>
2024-03-01 10:24:04 +0100

    Create exo1.java

---
+++ src/TP3/exo1.java
@@ -0,0 +1,26 @@
+package TP3;
+import java.util.Scanner;
+public class exo1 {
+    //declarer et initialiser un tableau puis affiche les elements du tableau
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Le nombre de lignes: ");
+        int a = s.nextInt();
+        System.out.print("Le nombre de colones: ");
+        int b = s.nextInt();
+        int[][] tableau= new int[a][b];
+        for(int i = 0;i < a;i++){
+            System.out.print("Ligne " + i + " :  ");
+            for(int j = 0;j < b; j++) tableau[i][j] = s.nextInt();
+        }
+        s.close();
+        for(int i = 0;i < a;i++){
+            System.out.print("| ");
+            for(int j = 0;j < b; j++) {
+                System.out.print(tableau[i][j]);
+            }
+            System.out.print(" |");
+        }
+
+    }
+}
